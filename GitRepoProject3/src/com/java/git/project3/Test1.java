93df8fdd6155402b6f83f8824ba42c8090a65344
Arvind <arvindnagar2912@gmail.com>
2022-11-14 21:15:42 +0530

    Myagain commit

---
+++ GitRepoProject3/src/com/java/git/project3/Test1.java
@@ -0,0 +1,17 @@
+package com.java.git.project3;
+
+import java.util.ArrayList;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		ArrayList arr = new ArrayList();
+		arr.add("Arvind");
+		arr.add("Nagar");
+		arr.add("Roshiya");
+		
+		System.out.println(arr);
+
+	}
+
+}
